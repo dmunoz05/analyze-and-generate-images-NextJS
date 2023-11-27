c3948327224bd076b5ff974dd896adddcf4ea7df
= <daniel20025febrero29@gmail.com>
2023-11-26 21:33:42 -0500

    Modify url NextJS

--- src/layout/footer.jsx
+++ src/layout/footer.jsx
@@ -4,7 +4,7 @@ function FooterLayout() {
     return (
         <div className='w-auto flex flex-row justify-center p-2 absolute bottom-0 left-0 right-0 bg-slate-900'>
             <p className="read-the-docs">
-                Created in <code><a target='__blank' href='https://reactjs.org/'>NextJS</a></code> by <code><a target='__blank' href='https://github.com/dmunoz05'>Daniel Muñoz</a></code> from <code><a target='__blank' href='https://github.com/dmunoz05?tab=repositories'>dmunoz05</a></code>
+                Created in <code><a target='__blank' href='https://nextjs.org/'>NextJS</a></code> by <code><a target='__blank' href='https://github.com/dmunoz05'>Daniel Muñoz</a></code> from <code><a target='__blank' href='https://github.com/dmunoz05?tab=repositories'>dmunoz05</a></code>
             </p>
         </div>
     )
